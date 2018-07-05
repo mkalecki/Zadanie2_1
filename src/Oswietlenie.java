3cf16a7045a3be88b0ee95ad65f84bb9e748bb6a
Michał Kalecki <mkalecki@gmail.com>
2018-07-05 15:30:38 +0200

    usunięte nieużywane fragmenty

--- src/Oswietlenie.java
+++ src/Oswietlenie.java
@@ -1,9 +1,6 @@
 public class Oswietlenie {
     public static void main(String[] args) {
         Lampa lampa1 = new Lampa ("nocna", 30, "czerwony");
-        lampa1.rodzaj = "nocna";
-        lampa1.moc = 30;
-        lampa1.kolor = "czerwony";
 
         System.out.println("LAMPAl:");
         System.out.println(lampa1.rodzaj);
@@ -12,9 +9,7 @@ public class Oswietlenie {
         System.out.println();
 
         Lampa lampa2 = new Lampa ("kinkiet", 15, "zimny");
-        lampa2.rodzaj = "kinkiet";
-        lampa2.kolor = "zimny";
-        lampa2.moc = 15;
+
         System.out.println("Lampa2:");
         System.out.println(lampa2.rodzaj);
         System.out.println(lampa2.moc + "W");
